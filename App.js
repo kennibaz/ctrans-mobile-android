286c960c301daa3c6c6d73f573ea2a2204fa3903
Bazyl <info@azbsupply.com>
2020-10-29 22:05:44 -0400

    PickupOrdersScreen created  0.0.4

--- App.js
+++ App.js
@@ -1,144 +1,23 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text,FlatList} from 'react-native';
+import {View, Text, FlatList} from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 import {ActivityIndicator, StyleSheet} from 'react-native';
+import {Provider as PaperProvider} from 'react-native-paper';
+
+import PickupOrdersScreen from "./screens/PickupOrdersScreen"
 const db = firestore();
 
 export default function App1() {
-  const [fsOrders, setFsOrders] = useState([]);
-  const [readyToLoad, setReadyToLoad] = useState(false);
-  const [loading, setLoading] = useState(true); // Set loading to true on component mount
-  const [users, setUsers] = useState([]); // Initial empty array of users
-
-  useEffect(() => {
-    const subscriber = firestore()
-      .collection('carriers-records')
-      .doc('c87U6WtSNRybGF0WrAXb')
-      .collection('orders')
-      .onSnapshot((querySnapshot ) => {
-        const orders = []
-        querySnapshot.forEach(documentSnapshot => {
-          orders.push({
-            ...documentSnapshot.data(),
-            key: documentSnapshot.id,
-          });
-        });
-  
-        setUsers(orders);
-        setLoading(false);
-      });
 
 
-    // Unsubscribe from events when no longer in use
-    return () => subscriber();
-  }, []);
 
-  if (loading) {
-    return (
-      <View style={{flex: 1}}>
-        <ActivityIndicator size="large" color="#00ff00" />
-      </View>
-    );
-  }
 
   return (
-    <View>
-      <Text>Deploy</Text>
-      <FlatList
-      data={users}
-      renderItem={({ item }) => (
-        <View style={{ height: 50, flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>User ID: {item.id}</Text>
-          <Text>User Name: {item.order_payment.order_total_amount}</Text>
-        </View>
-      )}
-    />
-    </View>
+    <PaperProvider>
+      <View>
+       <PickupOrdersScreen/>
+      </View>
+    </PaperProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  horizontal: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    padding: 10,
-  },
-});
-
-// useEffect(() => {
-//   const result = async () => {
-//     // const order_r = await db
-//     //   .collection('carriers-records')
-//     //   .doc('c87U6WtSNRybGF0WrAXb')
-//     //   .collection('orders')
-//     //   .get();
-//     // setFsOrders(order_r.docs);
-
-//     // const subs = firestore()
-//     //   .collection('carriers-records')
-//     //   .doc('c87U6WtSNRybGF0WrAXb')
-//     //   .collection('orders')
-//     //   .doc('gDEBAeJdYIesfP3G7RlB')
-//     //   .onSnapshot((doc) => {
-//     //     console.log(doc.data().created_at);
-
-//     //   });
-
-//     firestore().collection('carriers-records').doc('c87U6WtSNRybGF0WrAXb').collection('orders').get().then(querySnapshot=>{
-//       console.log("total orders", querySnapshot.size)
-//       querySnapshot.forEach(doc=>{
-//         console.log(doc.data())
-//       })
-//     })
-//   };
-//   result();
-
-//   // function onResult(QuerySnapshot) {
-//   //   console.log('Got Users collection result.', QuerySnapshot.docs);
-//   //   let new_array = []
-//   //   querySnapshot.forEach(function (doc) {
-
-//   //     let new_obj = {
-//   //       id: doc.id,
-//   //       data: doc.data(),
-//   //     };
-
-//   //     console.log(new_obj)
-
-//   //     new_array.push(new_obj)
-//   //     setFsOrders(new_array);
-//   //   });
-//   // }
-
-//   // function onError(error) {
-//   //   console.error(error);
-//   // }
-
-//   //  db.collection('Users').doc('c87U6WtSNRybGF0WrAXb')
-//   // .collection('orders').onSnapshot(onResult, onError);
-
-//   // var orderRef = db
-//   //   .collection('carriers-records')
-//   //   .doc('c87U6WtSNRybGF0WrAXb')
-//   //   .collection('orders');
-
-//   // orderRef.onSnapshot((querySnapshot) => {
-//   //   let new_array = [];
-//   //   querySnapshot.forEach(function (doc) {
-//   //     let new_obj = {
-//   //       id: doc.id,
-//   //       data: doc.data(),
-//   //     };
-//   //     setFsOrdersTest(doc.data());
-
-//   //     new_array.push(new_obj);
-//   //     // setFsOrders(new_array);
-//   //     setFsOrders((orders) => [...orders, new_obj]);
-//   //   });
-//   // });
-//   setReadyToLoad(true);
-// });
---
+++ screens/PickupOrdersScreen.js
@@ -0,0 +1,127 @@
+import React, {useState, useEffect} from 'react';
+import firestore from '@react-native-firebase/firestore';
+import {View, Text, StyleSheet, FlatList, ActivityIndicator, TouchableOpacity} from 'react-native';
+
+import {Card} from 'react-native-paper';
+const db = firestore();
+
+export default function PickupOrdersScreen() {
+  const [loading, setLoading] = useState(true); // Set loading to true on component mount
+  const [orders, setOrders] = useState([]); // Initial empty array
+  useEffect(() => {
+    const subscriber = firestore()
+      .collection('carriers-records')
+      .doc('c87U6WtSNRybGF0WrAXb')
+      .collection('orders')
+      .onSnapshot((querySnapshot) => {
+        const orders = [];
+        querySnapshot.forEach((documentSnapshot) => {
+          orders.push({
+            ...documentSnapshot.data(),
+            key: documentSnapshot.id,
+          });
+        });
+
+        setOrders(orders);
+        setLoading(false);
+      });
+
+    // Unsubscribe from events when no longer in use
+    return () => subscriber();
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={{flex: 1}}>
+        <ActivityIndicator size="large" color="#00ff00" />
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <FlatList
+        data={orders}
+        renderItem={({item}) => (
+            <View style={styles.screen} >
+            <Card style={styles.card}>
+              <Card.Content>
+                <View>
+                  <TouchableOpacity
+                    onPress={() => {
+                      props.navigation.navigate('OrderDetails', {
+                        order_inner_id: order.data.order_shipper_inner_id,
+                        order_id: order.id,
+                        order_data: order.data,
+                      });
+                    }}>
+                    <View>
+                      <View id="row_1" style={styles.row}>
+                        <Text style={{fontWeight: '700'}}>
+                          {item.order_shipper_inner_id}
+                        </Text>
+                        <Text>
+                          $ {item.order_payment.order_total_amount}
+                        </Text>
+                      </View>
+                      <View id="row_2" style={styles.row}>
+                        <Text>
+                          {item.vehiclesArray.year}{' '}
+                          {item.vehiclesArray.make}{' '}
+                          {item.vehiclesArray.model}
+                        </Text>
+                      </View>
+                      <View id="row_3" style={styles.row}>
+                        <Text>
+                          {item.pickup.pickup_address.city},{' '}
+                          {item.pickup.pickup_address.state}{' '}
+                          {item.pickup.pickup_address.zip}{' '}
+                        </Text>
+                        <Text>
+                          {item.pickup.pickup_scheduled_first_date}
+                        </Text>
+                      </View>
+                      <View id="row_4" style={styles.row}>
+                        <Text>
+                          {item.delivery.delivery_address.city},{' '}
+                          {item.delivery.delivery_address.state}{' '}
+                          {item.delivery.delivery_address.zip}{' '}
+                        </Text>
+                        <Text>
+                          {item.delivery.delivery_scheduled_first_date}
+                        </Text>
+                      </View>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+              </Card.Content>
+            </Card>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+    screen: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    row: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    card: {
+      padding: 2,
+      marginBottom: 4,
+      marginTop: 4,
+      width: '97%',
+      borderRadius: 5,
+      borderLeftWidth: 4,
+      borderColor: 'blue',
+    },
+  });
+  
