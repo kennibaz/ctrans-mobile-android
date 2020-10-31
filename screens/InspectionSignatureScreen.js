9b2d962f60365a27eeadb9af37807bebbb378ce5
Bazyl <info@azbsupply.com>
2020-10-31 17:12:58 -0400

    upload of pictures and signature, navigation back to picked up  0.0.14

--- screens/InspectionSignatureScreen.js
+++ screens/InspectionSignatureScreen.js
@@ -26,42 +26,75 @@ export default function InspectionSignatureScreen({route, navigation}) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [signatureUri, setSignatureUri] = useState('');
+  const [signatureUploadedUri, setSignatureUploadedUri] = useState('');
   const [readyToSave, setReadyToSave] = useState(false);
+  const [initialImageArrayLength, setInitialImageArrayLength] = useState(false);
+  const [uploadedImages, setUploadedImages] = useState([]);
+  const [readyToFsUpload, setReadyToFsUpload] = useState(false);
+  const [odometer, setOdometer] = useState(false);
+  const [pickupNotes, setPickupNotes] = useState(false);
+  const [uploadDone, setUploadDone] = useState(false);
 
   const onPickupHandler = async () => {
-    // db.collection('carriers-records')
-    //   .doc('c87U6WtSNRybGF0WrAXb')
-    //   .collection('orders')
-    //   .doc(route.params.order_id)
-    //   .update({
-    //     order_status: 'Picked',
-    //   });
-
     const foundOrder = pickupOrders.filter((order) => {
       return order.key === route.params.order_id;
     });
 
     const imagesArray = foundOrder[0].imageSet;
     console.log(imagesArray);
+    setOdometer(foundOrder[0].odometer);
+    setPickupNotes(foundOrder[0].driver_pickup_notes);
+    setInitialImageArrayLength(imagesArray.length);
+
+    //save a pic of a signature
+    const newSignUuid = uuid();
+    const reference = storage().ref(`signature-${newSignUuid}.jpg`);
+    const pathToFile = signatureUri;
+    await reference.putFile(pathToFile);
+    setSignatureUploadedUri(
+      await storage().ref(`signature-${newSignUuid}.jpg`).getDownloadURL(),
+    );
 
     imagesArray.forEach(async (image) => {
       const newId = uuid();
       const reference = storage().ref(`inspection-photo-${newId}.jpg`);
       const pathToFile = image.mergedImage;
       await reference.putFile(pathToFile);
-
-      // setUploadedImages((currentImages) => [...currentImages, linkToUpload]);
+      const url = await storage()
+        .ref(`inspection-photo-${newId}.jpg`)
+        .getDownloadURL();
+      setUploadedImages((currentImages) => [...currentImages, url]);
     });
-
-    // uploads file
-    
   };
+
+  useEffect(() => {
+    if (uploadedImages.length === initialImageArrayLength) {
+      db.collection('carriers-records')
+        .doc('c87U6WtSNRybGF0WrAXb')
+        .collection('orders')
+        .doc(route.params.order_id)
+        .update({
+          order_status: 'Picked',
+          'pickup.pickup_conditions.name_on_pickup_signature': name,
+          'pickup.pickup_conditions.email_on_pickup_signature': email,
+          'pickup.pickup_conditions.odometer': odometer,
+          'pickup.pickup_conditions.driver_pickup_notes': pickupNotes,
+          'pickup.pickup_conditions.pickup_inspection_images_links': uploadedImages,
+          'pickup.pickup_conditions.signature_image_link': signatureUploadedUri,
+        });
+        setUploadDone(true)
+    }
+  }, [uploadedImages]);
+
+  useEffect(() => {
+    if (uploadDone) {
+      navigation.navigate('PickupOrders');
+    }
+  }, [uploadDone]);
 
   const handleSignature = async (signature) => {
     const newId = uuid();
-
     let correctedSignature = signature.replace('data:image/png;base64,', '');
-
     let path = RNFS.DocumentDirectoryPath + newId + '-signature.jpg';
     const signatureFile = await RNFS.writeFile(
       path,
